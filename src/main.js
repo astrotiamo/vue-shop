a42ee620c699e61cf9f16e317450a5f4851d91a9
astrotiamo <1308565350@qq.com>
2021-08-14 09:24:03 +0800

    login

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// import ElementUI from 'element-ui'
+import './plugins/element'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+
 Vue.config.productionTip = false
+// Vue.use(ElementUI)
 
 new Vue({
   router,
